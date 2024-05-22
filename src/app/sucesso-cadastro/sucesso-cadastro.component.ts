98db9682fe73fa9b119805b09664ba6f393d0304
eduardoaalmeidaa <almeidaeduardu@gmail.com>
2024-05-22 19:58:07 -0300

    f1 commit

---
+++ src/app/sucesso-cadastro/sucesso-cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-sucesso-cadastro",
+  templateUrl: "./sucesso-cadastro.component.html",
+  styleUrls: ["./sucesso-cadastro.component.css"],
+})
+export class SucessoCadastroComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
